a475270b07b57fdd958fc2c3dc6d42dbe92bd4be
KopaczP <kopaczkapl@gmail.com>
2022-04-17 12:56:55 +0200

    imports

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:gallium_flutter/navigation/app_router.gr.dart';
-import 'package:gallium_flutter/pages/page_b.dart';
-import 'package:gallium_flutter/widgets/bottom_nav_bar.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({
